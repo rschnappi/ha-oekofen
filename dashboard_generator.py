73e7f8b71c4be5cd3179469740a1b59effaf72b8
Roland Schnabl <r.schnappi@gmail.com>
2026-04-03 08:58:01 +0200

    Update dashboard_generator.py to use the correct path to sensor.py

---
+++ dashboard_generator.py
@@ -0,0 +1,9 @@
+import os
+
+# Get the directory of the current script
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Updated path to sensor.py
+sensor_path = os.path.join(current_dir, 'sensor.py')
+
+# Rest of the dashboard_generator.py code
